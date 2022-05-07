8708fcc9b43c1bd96cc4c8cf9f18ecf495560c33
AMAURY <amauryfranssen@gmail.com>
2022-05-07 15:46:30 +0200

    feat: fetching data from json

--- public/script.js
+++ public/script.js
@@ -1,14 +1,34 @@
-fetch("../server/Schools Survey for Giga.json")
-    .then(Response => Response.json())
-    .then(data => {
-        console.log(data);
-    });
 
-// 13.8834, -60.9879
-let map = L.map('map').setView([13.8834, -60.9860], 10);
-let marker = L.marker([13.8834, -60.9879]).addTo(map);
- 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+async function getData() {
+  return await new Promise((resolve, reject) => {
+    fetch("../server/Schools Survey for Giga.json")
+
+     .then(response => {
+        // console.log(response);
+       return response.json();
+
+        }).then(data_from_fetched => {
+         let data = data_from_fetched;
+         console.log(data_from_fetched);
+         resolve(data_from_fetched);
+   })
+ })    
+}
+console.log("hello");
+async function doto() {
+   console.log("hello2");
+
+  let map = L.map('map').setView([13.8834, -60.9860], 10);
+  titleL(map);
+  let books = await getData();
+  process_array(books, map);
+};
+
+console.log(await doto())
+
+function titleL(map){
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
@@ -16,4 +36,20 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWFwZGF5IiwiYSI6ImNsMnZpMG5tdjBjM3kzanBob3FncDF1bm8ifQ.4JAtmIF1-atjwiGwrKTWmQ',
 }).addTo(map);
+
+    return map;
+}
+
+
+function process_array(books, map) {
+    for (let i = 0; i < books.length; i++) {
+        let name1 = books[i].name
+        let address1 = books[i].address
+        let shelter = books[i].Shelter_tsunami
+        L.marker([books[i].latitude, books[i].longitude]).addTo(map)
+            .bindPopup(name1+'<br/>'+address1+'<br/>'+'shelter for tsunami:'+shelter)
+            .openPopup();
+            
+    }
+}
 
--- server/test.py
+++ server/test.py
@@ -1,3 +1,4 @@
 import excel2json
 
 excel2json.convert_from_file('saint_lucia.xlsx')
+
