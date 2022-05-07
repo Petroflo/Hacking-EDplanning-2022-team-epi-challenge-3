c0b8542ba6c68b826fd37ea83ba2c9ba28db5a99
AMAURY <amauryfranssen@gmail.com>
2022-05-07 15:51:45 +0200

    date.json ok

--- server/test.py
+++ server/test.py
@@ -2,4 +2,4 @@ import excel2json
 import os
 
 excel2json.convert_from_file('saint_lucia.xlsx')
-os.rename("Schools Survey for Giga.json", "Data")
+os.rename("Schools Survey for Giga.json", "Data.json")
