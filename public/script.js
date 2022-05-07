57ce06c270c5fedb36f367b6804f26f2bd1b8571
AMAURY <amauryfranssen@gmail.com>
2022-05-07 12:11:57 +0200

    new

--- public/script.js
+++ public/script.js
@@ -1,3 +1,18 @@
+
+// let reponseData = fetch('./Schools Survey for Giga.json')
+//   .then( response => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error: ${response.status}`);
+//     }
+//     return response.json();
+//   })
+// //   .then( json => initialize(json) )
+//   .catch( err => console.error(`Fetch problem: ${err.message}`) );
+
+// let mydata = JSON.parse(reponseData);
+// alert(mydata);
+
+
 // 13.8834, -60.9879
 let map = L.map('map').setView([13.8834, -60.9860], 10);
 let marker = L.marker([13.8834, -60.9879]).addTo(map);
@@ -10,3 +25,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWFwZGF5IiwiYSI6ImNsMnZpMG5tdjBjM3kzanBob3FncDF1bm8ifQ.4JAtmIF1-atjwiGwrKTWmQ',
 }).addTo(map);
+
